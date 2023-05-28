db968f4de13909c10684b1dbb1c1939dde4c700d
Kong Sang Uk <qkqhfp19@naver.com>
2023-05-28 12:20:44 +0900

    논리적인 사고력 if - else if

--- src/IfBasic/IfElseIfTest.java
+++ src/IfBasic/IfElseIfTest.java
@@ -1,29 +1,30 @@
 package IfBasic;
 
-import java.util.Scanner;
-
 public class IfElseIfTest {
     public static void main(String[] args) {
 
-        // Q. 정수 1개를 입력 받아 짝수인지 홀수 인지를 판단하는 프로그램을 만드세요.(IfElseTest.java)
-        Scanner scan = new Scanner(System.in);
+        // Q. 80점 이상의 학생 중에서 90점 이상은 A반, 85점 이상은 B반,
+        //    그렇지 않으면 C반으로 편성하고 점수가 80미만이면 불합격 처리하는 프로그램을 작성하세요.
+        // 단, 점수는 0 ~ 100점 사이값을 받는다.
 
-        int su = scan.nextInt();
+        int jumsu = 80;
+        if (jumsu>=0 && jumsu<=100){
 
-        if (su%2 == 0){
-            System.out.print("짝수입니다.");
+            if (jumsu >= 80) {
+                if (jumsu >= 90) {
+                    System.out.print("A반");
+                } else if (jumsu >= 85) {
+                    System.out.print("B반");
+                } else if (jumsu >= 80) {
+                    System.out.print("C반");
+                } else {
+                    System.out.print("불합격");
+                }
+            }else {
+                System.out.println("학생의 점수는 80점 이상이 아닙니다.");
+            }
         }else {
-            System.out.println("홀수입니다.");
+            System.out.println("유효한 점수가 아닙니다.");
         }
-
-        // Q. 입력 받은 수가 12의 배수인지를 출력하는 코드를 작성하세요.
-        System.out.println("정수를 입력하세요 : ");
-        int num = scan.nextInt();
-        if (num%12 == 0){
-            System.out.println("12의 배수입니다.");
-        }else {
-            System.out.println("12의 배수가 아닙니다.");
-        }
-
     }
 }
