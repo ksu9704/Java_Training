02fb639599605c2977d8e4818bf40d2ffea795e7
Kong Sang Uk <qkqhfp19@naver.com>
2023-05-28 12:33:43 +0900

    switch- case

---
+++ src/SwitchCase/SwitchCaseTest.java
@@ -0,0 +1,26 @@
+package SwitchCase;
+
+public class SwitchCaseTest {
+    public static void main(String[] args) {
+
+        // Q. switch ~ case 문을 활용하여 요일에 해당하는 운동을 출력하세요.(SwitchCaseTest.java)
+
+        String day = "Wednesday";
+
+        switch (day) {
+            case "Sunday":
+                System.out.println("야구");
+                break;
+            case "Monday":
+                System.out.println("농구");
+                break;
+            case "Tuesday":
+            case "Wednesday":
+                System.out.println("수영");
+                break;
+            default:
+                System.out.println("휴식");
+                break;
+        }
+    }
+}
