394a4d4bd2c95d75f6d79e21ed8203325b206a52
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-02 23:35:03 +0900

    AccessModifier

---
+++ src/model/MemberDOT.java
@@ -0,0 +1,11 @@
+package model;
+
+public class MemberDOT {
+    public String name;
+    private int age;
+    public String phone;
+
+    public void payly(){
+        System.out.println("운동을 한다.");
+    }
+}
---
+++ src/part3/AccessModifier.java
@@ -0,0 +1,13 @@
+package part3;
+
+import model.MemberDOT;
+
+public class AccessModifier {
+    public static void main(String[] args) {
+        MemberDOT memberDOT = new MemberDOT();
+        memberDOT.name = "공상욱";
+        memberDOT.phone = "010-4228-9999";
+        System.out.println(memberDOT.name + "\t" + memberDOT.phone + "\t");
+        memberDOT.payly();
+    }
+}
