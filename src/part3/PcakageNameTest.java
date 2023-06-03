ae573c9d1103493216c5456d99a641909f631ba0
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-03 23:07:58 +0900

    클래스를 접근하는 이름 이해

---
+++ src/part3/PcakageNameTest.java
@@ -0,0 +1,19 @@
+package part3;
+
+import model.CarDAO;
+import model.CarDTO;
+
+import java.util.Scanner;
+
+public class PcakageNameTest {
+    public static void main(String[] args) {
+        // Q. 지금까지 자바에서 제공해주어서 사용해본 클래스들의 이름을 적어보자?
+        String str;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello World");
+
+        // Q. 우리가 직접 만들어서 사용해 본 클래스들의 이름을 적어보자?
+        CarDTO carDTO = new CarDTO();
+        CarDAO carDAO = new CarDAO();
+    }
+}
