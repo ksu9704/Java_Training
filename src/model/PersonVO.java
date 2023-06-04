d5c6aeca7346226726f48d7c4821bb805fb1a6f0
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-04 20:05:45 +0900

    생성자를 이용한 객체 초기화

--- src/model/PersonVO.java
+++ src/model/PersonVO.java
@@ -4,7 +4,19 @@ public class PersonVO {
     private String name;
     private int age;
     private String phone;
+    // 생략된 생상자 메서드가 있다. => 기본생성
 
+    public PersonVO(){
+        // 객체를 생성하는 코드는 내부에서 만들어진다.
+        // 객체의 초기화를 한다.
+    }
+
+    // 생성자의 오버로딩
+    public PersonVO(String name, int age, String phone){
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
     // SET
     public void setName(String name) {
         this.name = name;
