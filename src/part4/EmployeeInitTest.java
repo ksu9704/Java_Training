ff810df463f331ef7927f3bff4fe222e51a0b39f
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-07 15:34:08 +0900

    Employee

---
+++ src/part4/EmployeeInitTest.java
@@ -0,0 +1,8 @@
+package part4;
+
+public class EmployeeInitTest {
+    public static void main(String[] args) {
+        RempVO rempVO = new RempVO("공상욱", 33, "010-1111-3333", "2022-10-10", "개발부", false);
+        System.out.println(rempVO.toString());
+    }
+}
