118b5d7da982934cbd623606aed5722943488458
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-09 22:22:44 +0900

    PolyMethodTest

---
+++ src/part4/Poly/PolyMethodTest.java
@@ -0,0 +1,31 @@
+package part4.Poly;
+
+import part4.DogCatTest.Animal;
+import part4.DogCatTest.Cat;
+import part4.DogCatTest.Dog;
+
+public class PolyMethodTest {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        display(d);
+        Cat c = new Cat();
+        display(c);
+    }
+
+    private static void display(Animal ani) { // 다형성인수
+        ani.eat();
+
+        if (ani instanceof Cat){
+        ((Cat)ani).night();
+        }
+    }
+
+//    private static void display(Dog d) {
+//        d.eat();
+//    }
+//
+//    private static void display(Cat c) {
+//        c.eat();
+//    }
+
+}
