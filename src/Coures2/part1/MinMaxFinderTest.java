34586b269a0c1a1e4b30dcb39b142b7b1e12e655
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-11 21:05:40 +0900

    MinMaxFinder

---
+++ src/Coures2/part1/MinMaxFinderTest.java
@@ -0,0 +1,15 @@
+package Coures2.part1;
+
+import Coures2.model2.MinMaxFinder;
+
+public class MinMaxFinderTest {
+    public static void main(String[] args) {
+        int[] arr = {4,3,1,2,5};
+        int min = MinMaxFinder.findMin(arr);
+        int max = MinMaxFinder.findMax(arr);
+
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+
+    }
+}
