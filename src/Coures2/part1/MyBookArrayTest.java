292de40d1a6d775d755a3314e573a51914ca1379
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-12 20:38:26 +0900

    BookArrayTest

---
+++ src/Coures2/part1/MyBookArrayTest.java
@@ -0,0 +1,27 @@
+package Coures2.part1;
+
+import Coures2.model2.Book;
+import Coures2.model2.BookArray;
+
+public class MyBookArrayTest {
+    public static void main(String[] args) {
+        BookArray list = new BookArray();
+        list.add(new Book("Java", 15000, "한빛", "공길동"));
+        list.add(new Book("C++", 13000, "대림", "공길이"));
+        list.add(new Book("Python", 17000, "정보", "공동길"));
+
+//        Book vo = list.get(0);
+        System.out.println(list.get(0));
+
+//        vo = list.get(1);
+        System.out.println(list.get(1));
+
+//        vo = list.get(2);
+        System.out.println(list.get(2));
+
+        for (int i=0; i< list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+    }
+}
