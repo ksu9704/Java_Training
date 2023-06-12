4d2689a7cf80d657f475c5e94da35ed756aff221
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-12 19:55:08 +0900

    IntArrayBasic

---
+++ src/Coures2/part1/IntArrayBasic.java
@@ -0,0 +1,25 @@
+package Coures2.part1;
+
+public class IntArrayBasic {
+    public static void main(String[] args) {
+        // 종수 5개를 배열에 저장하고 출력하세요.
+        int[] nums = new int[5];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums[3] = 4;
+        nums[4] = 5;
+
+        // num.length : 길이를구하는 동작
+        for (int i=0; i<nums.length; i++){
+            int data = nums[i]; // 데이터를 얻는동작(가져오는동작)
+            System.out.println(data);
+        }
+
+        // 향상된 for문(foreach)
+        for (int data : nums){
+            System.out.println(data);
+
+        }
+    }
+}
