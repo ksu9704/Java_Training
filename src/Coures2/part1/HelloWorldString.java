4adbeb907a3f7ac5920d2fba29847e087a024ad9
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-13 22:04:35 +0900

    HelloWorldString

---
+++ src/Coures2/part1/HelloWorldString.java
@@ -0,0 +1,12 @@
+package Coures2.part1;
+
+public class HelloWorldString {
+    public static void main(String[] args) {
+        String str = new String("HelloWorld");
+        System.out.println(str.toString());
+
+        String str1 = "HelloWorld";
+        System.out.println(str1);
+
+    }
+}
