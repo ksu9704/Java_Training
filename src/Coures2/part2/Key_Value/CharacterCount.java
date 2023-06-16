8492306c6be625bd68e3c12b1e708173ef3e66f0
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-16 22:07:15 +0900

    Key-Value로 데이터를 관리

--- src/Coures2/part2/Key_Value/CharacterCount.java
+++ src/Coures2/part2/Key_Value/CharacterCount.java
@@ -1,8 +1,26 @@
 package Coures2.part2.Key_Value;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CharacterCount {
     public static void main(String[] args) {
 
+        String str = "Hello, World";
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        char[] setArray = str.toCharArray();
+
+        for (char c : setArray){
+            if (charCountMap.containsKey(c)){
+                charCountMap.put(c, charCountMap.get(c)+1);
+            }else {
+                charCountMap.put(c, 1);
+            }
+        }
+        System.out.println("Character Count");
+        for (char c : charCountMap.keySet()){
+            System.out.println(c + ":" + charCountMap.get(c));
+        }
 
     }
 }
