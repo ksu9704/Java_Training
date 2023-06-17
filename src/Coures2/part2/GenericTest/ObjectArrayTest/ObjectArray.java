06e67bb9c5cc9957c55a4b412b766673543e4c38
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-17 20:39:15 +0900

    ObjectArray

---
+++ src/Coures2/part2/GenericTest/ObjectArrayTest/ObjectArray.java
@@ -0,0 +1,23 @@
+package Coures2.part2.GenericTest.ObjectArrayTest;
+
+public class ObjectArray<T> {
+    private T[] array;
+    private int size;
+
+    public ObjectArray(int size){
+        array = (T[]) new Object[size];
+    }
+
+    public void set(int index, T value){
+        array[index] = value;
+        size++;
+    }
+
+    public T get(int index){
+        return array[index];
+    }
+
+    public int size(){
+        return size;
+    }
+}
