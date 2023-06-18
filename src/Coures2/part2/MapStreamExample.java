69ca9ad54538ee7b33d7ca9e56fc489c375237e5
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-18 19:14:04 +0900

    Stream API의 활용

---
+++ src/Coures2/part2/MapStreamExample.java
@@ -0,0 +1,18 @@
+package Coures2.part2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapStreamExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        //스트림의각원소를제곱한값으로구성된새로운리스트생성
+        List<Integer> squaredNumbers = numbers.stream().map(n -> n * n)
+                .collect(Collectors.toList());
+
+        System.out.println("제곱한 값의 리스트:" + squaredNumbers);
+    }
+}
+
