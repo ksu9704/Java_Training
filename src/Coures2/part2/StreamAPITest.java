b6b9c8d344ec2370ce66447a1eeee22c9cf00703
Kong Sang Uk <qkqhfp19@naver.com>
2023-06-18 19:05:16 +0900

    Stream API

---
+++ src/Coures2/part2/StreamAPITest.java
@@ -0,0 +1,33 @@
+package Coures2.part2;
+
+import java.util.Arrays;
+
+public class StreamAPITest {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+        /*int even = 0;
+        for (int num : numbers){
+            if (num%2==0){
+                even+=num;
+            }
+        }*/
+
+//        IntStream stream = Arrays.stream(numbers);
+
+        int sumOfEvens = Arrays.stream(numbers)
+                        .filter(n->n%2==0)
+                                .sum();
+
+        System.out.println("sumOfEvens = " + sumOfEvens);
+
+
+        int[] evenNumbers = Arrays.stream(numbers)
+                .filter(n->n%2==0)
+                .toArray();
+
+        for (int even : evenNumbers){
+            System.out.println("even : " + even);
+        }
+
+    }
+}
