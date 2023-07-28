246ce643633231f079bb01b88ad9a017918e4cba
Kong Sang Uk <qkqhfp19@naver.com>
2023-07-28 22:57:36 +0900

    coding test

---
+++ src/JavaCodingTest/Ch01_문자열/문서검색/Main.java
@@ -0,0 +1,26 @@
+package JavaCodingTest.Ch01_문자열.문서검색;
+
+import java.util.Scanner;
+
+class Main
+{
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String doc = sc.nextLine();
+        String word = sc.nextLine();
+
+        int startIndex = 0;
+        int count = 0;
+
+        while (true) {
+            int findIndex = doc.indexOf(word, startIndex);
+            if (findIndex < 0)
+                break;
+
+            startIndex = findIndex + word.length();
+            count++;
+        }
+        System.out.println(count);
+    }
+}
