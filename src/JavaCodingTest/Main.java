a740ac618b2869fca809b9035c44225cd38f078e
Kong Sang Uk <qkqhfp19@naver.com>
2023-08-23 23:32:20 +0900

    coding test

--- src/JavaCodingTest/Main.java
+++ src/JavaCodingTest/Main.java
@@ -6,6 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String str = sc.next();
+        String[] arr = new String[Integer.parseInt(str)];
+        System.out.println(arr);
 
     }
 }
