66731e15880ec8f34bafc13d3a0327d92d851b29
Kong Sang Uk <qkqhfp19@naver.com>
2023-08-25 23:12:06 +0900

    coding test

--- src/JavaCodingTest/Main.java
+++ src/JavaCodingTest/Main.java
@@ -4,11 +4,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        String str = sc.next();
-        String[] arr = new String[Integer.parseInt(str)];
-        System.out.println(arr);
+        int answer = 0;
+        int num = sc.nextInt();
+        int n = sc.nextInt();
+
+        answer = num % n;
+
+        if (answer == 0){
+            System.out.println(0);
+        }
+
+        if (answer != 0){
+            System.out.println(1);
+        }
+
+
 
     }
 }
---
+++ src/JavaCodingTest/문자열곱하기/Main.java
@@ -0,0 +1,19 @@
+package JavaCodingTest.문자열곱하기;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String my_string = sc.next();
+        String answer = "";
+        int k = sc.nextInt();
+
+        for (int i = 0; i < k; i++) {
+            answer += my_string;
+        }
+        System.out.println(answer);
+    }
+}
